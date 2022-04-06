1b34a9b0a1e1d29a47055fe6192b8a474893b9e7
Leonardo Blanski <leonardo.blanski@giver.com.br>
2022-04-06 12:51:17 -0300

    creating a base widget to use in other widgets

--- lib/base_widget.dart
+++ lib/base_widget.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons_null_safety/flutter_icons_null_safety.dart';
+import 'package:relax_sounds/shared/app_colors.dart';
 
 class BaseWidget extends StatelessWidget {
   final String title;
   final Widget body;
+  final AppBar? appbar;
 
   const BaseWidget({
     required this.title,
     required this.body,
+    this.appbar,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
+      appBar: appbar ?? _getAppBar(),
       body: body,
+    );
+  }
+
+  AppBar _getAppBar() {
+    return AppBar(
+      backgroundColor: appColorTheme,
+      title: Text(title),
+      elevation: 0,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Ionicons.ios_settings,
+            color: Colors.white,
+          ),
+        ),
+      ],
     );
   }
 }
